6fc25eb717889f2a728313a455a02e5c80bc58c4
Marzeu <marzahell@hotmail.com>
2021-11-07 12:20:10 -0300

    Crud scaffolding de departamentos

---
+++ Ponto/Data/PontoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ponto.Models;
+
+namespace Ponto.Data
+{
+    public class PontoContext : DbContext
+    {
+        public PontoContext (DbContextOptions<PontoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ponto.Models.Departamento> Departamento { get; set; }
+    }
+}
